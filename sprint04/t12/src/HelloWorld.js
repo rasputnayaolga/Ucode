4e7ad436281c650f322c037dbfa8eebbcf353063
Rasputnaya Olga <69202187+rasputnayaolga@users.noreply.github.com>
2021-04-25 20:17:56 +0300

    Add files via upload

---
+++ sprint04/t12/src/HelloWorld.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// const HelloWorld = () => {
+  
+//   function sayHello() {
+//     alert('Hello, World!');
+//   }
+  
+//   return (
+//     <button onClick={sayHello}>Click me!</button>
+//   );
+// };
+
+// export default HelloWorld;
+
+import React from 'react';
+
+const HelloWorld = () => {
+function HelloReact(){}
+    return (
+      <h1>Hello React!</h1>
+    )
+};
+
+export default HelloWorld;
